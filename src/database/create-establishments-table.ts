543cb33e19a2b2d9f7d5b12c631441ae2493f6e6
guialveess <97guilherme.alves@gmail.com>
2025-05-14 13:26:52 -0300

    :bricks: ci: criação da Tabela estabelecimentos usando DynamoDB

---
+++ src/database/create-establishments-table.ts
@@ -0,0 +1,38 @@
+import { DynamoDBClient, CreateTableCommand } from '@aws-sdk/client-dynamodb';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+  endpoint: process.env.DYNAMODB_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+async function createTable() {
+  try {
+    const command = new CreateTableCommand({
+      TableName: 'Establishments',
+      AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+      KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5,
+      },
+    });
+
+    await client.send(command);
+    console.log('Tabela "Establishments" criada com sucesso');
+  } catch (err: any) {
+    if (err.name === 'ResourceInUseException') {
+      console.log('A tabela "Establishments" já existe.');
+    } else {
+      console.error(' Erro ao criar tabela:', err);
+    }
+  }
+}
+
+createTable();
