b7bac5429384500e8e3c992f18dfe4bf1cb837b5
guialveess <97guilherme.alves@gmail.com>
2025-05-14 13:28:04 -0300

    :bricks: ci: criação da Tabela Regras de Estabelecimentos usando DynamoDB

---
+++ src/database/create-establishment-rules-table.ts
@@ -0,0 +1,38 @@
+import { DynamoDBClient, CreateTableCommand } from '@aws-sdk/client-dynamodb';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+  endpoint: process.env.DYNAMODB_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+async function createTable() {
+  try {
+    const command = new CreateTableCommand({
+      TableName: 'EstablishmentRules',
+      AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+      KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5,
+      },
+    });
+
+    await client.send(command);
+    console.log('Tabela "EstablishmentRules" criada com sucesso');
+  } catch (err: any) {
+    if (err.name === 'ResourceInUseException') {
+      console.log('A tabela "EstablishmentRules" já existe.');
+    } else {
+      console.error('Erro ao criar tabela:', err);
+    }
+  }
+}
+
+createTable();
